58322847647400da55188e2a8b611c505e00d719
0xClandestine <clandestine.eth@proton.me>
2024-01-23 17:06:15 -0500

    nit: cleanup

--- src/utils/BaseERC721.sol
+++ src/utils/BaseERC721.sol
@@ -1,9 +1,11 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.0;
 
+import {SSTORE2, Initializable} from "lib/solady/src/Milady.sol";
+
 /// @notice Modern, minimalist, and gas efficient ERC-721 implementation.
 /// @author Modified from Solmate (htps://github.com/transmissions11/solmate/blob/main/src/tokens/ERC721.sol)
-abstract contract BaseERC721 {
+abstract contract BaseERC721 is Initializable {
     /// -----------------------------------------------------------------------
     /// Errors
     /// -----------------------------------------------------------------------
@@ -42,28 +44,87 @@ abstract contract BaseERC721 {
     /// Mutables
     /// -----------------------------------------------------------------------
 
+    mapping(uint256 id => bytes32 data) internal _tokenData;
+
+    mapping(address owner => uint256 balance) internal _accountData;
+
     mapping(uint256 id => address spender) public getApproved;
 
     mapping(address owner => mapping(address spender => bool approved)) public
         isApprovedForAll;
 
     /// -----------------------------------------------------------------------
+    /// Immutables
+    /// -----------------------------------------------------------------------
+
+    function _INITIAL_HOLDERS_POINTER()
+        internal
+        pure
+        virtual
+        returns (address);
+
+    function _INITIAL_HOLDERS_LENGTH()
+        internal
+        view
+        virtual
+        returns (uint256 result);
+        
+    /// -----------------------------------------------------------------------
+    /// Setup
+    /// -----------------------------------------------------------------------
+
+    function initialize(bytes calldata addresses)
+        external
+        virtual
+        initializer
+    {
+        unchecked {
+            uint256 n = addresses.length / 20;
+            for (uint256 i; i < n; ++i) {
+                emit Transfer(
+                    address(0),
+                    address(bytes20(addresses[i * 20:i * 20 + 20])),
+                    i
+                );
+            }
+        }
+    }
+
+    /// -----------------------------------------------------------------------
     /// Read-Only Accounting
     /// -----------------------------------------------------------------------
 
-    function ownerOf(uint256 id) public view virtual returns (address owner);
+    function ownerOf(uint256 id) public view virtual returns (address owner) {
+        if ((owner = _safeOwnerOf(id)) == address(0)) {
+            revert NOT_MINTED();
+        }
+    }
 
     function balanceOf(address owner)
         public
         view
         virtual
-        returns (uint256 balance);
+        returns (uint256 balance)
+    {
+        if (owner == address(0)) revert ZERO_ADDRESS();
+        return _safeBalanceOf(owner);
+    }
 
     /// -----------------------------------------------------------------------
     /// Actions
     /// -----------------------------------------------------------------------
 
-    function approve(address spender, uint256 id) public virtual;
+    function approve(address spender, uint256 id) public virtual {
+        address owner = ownerOf(id);
+
+        if (!(msg.sender == owner || isApprovedForAll[owner][msg.sender])) {
+            revert NOT_AUTHORIZED();
+        }
+
+        getApproved[id] = spender;
+
+        emit Approval(owner, spender, id);
+    }
 
     function setApprovalForAll(address operator, bool approved)
         public
@@ -76,7 +137,42 @@ abstract contract BaseERC721 {
 
     function transferFrom(address from, address to, uint256 id)
         public
-        virtual;
+        virtual
+    {
+        if (!(from == ownerOf(id))) revert WRONG_FROM();
+
+        if (to == address(0)) revert INVALID_RECIPIENT();
+
+        if (
+            !(
+                msg.sender == from || isApprovedForAll[from][msg.sender]
+                    || msg.sender == getApproved[id]
+            )
+        ) {
+            revert NOT_AUTHORIZED();
+        }
+
+        // Underflow of the sender's balance is impossible because we check for
+        // ownership above and the recipient's balance can't realistically overflow.
+        unchecked {
+            bytes32 tokenData = _tokenData[id];
+
+            bool neverSent = tokenData[0] == 0;
+
+            if (neverSent && id < _INITIAL_HOLDERS_LENGTH()) {
+                _tokenData[id] = _setLeadingByte(bytes32(bytes20(to)), 0xFF);
+            } else {
+                _tokenData[id] = bytes32(bytes20(to));
+                _accountData[from]--;
+            }
+        }
+
+        _accountData[to]++;
+
+        delete getApproved[id];
+
+        emit Transfer(from, to, id);
+    }
 
     function safeTransferFrom(address from, address to, uint256 id)
         public
@@ -97,12 +193,100 @@ abstract contract BaseERC721 {
     }
 
     /// -----------------------------------------------------------------------
+    /// Read-Only Accounting Helpers
+    /// -----------------------------------------------------------------------
+
+    function _safeOwnerOf(uint256 id)
+        internal
+        view
+        virtual
+        returns (address owner);
+
+    function _safeBalanceOf(address owner)
+        internal
+        view
+        virtual
+        returns (uint256 balance);
+
+    function _searchSorted(address pointer, uint256 needle, uint8 size)
+        internal
+        virtual
+        view
+        returns (bool found, uint256 index)
+    {
+        unchecked {
+            uint256 length = _INITIAL_HOLDERS_LENGTH();
+            uint256 h = length;
+            uint256 l = 0;
+
+            while (l < h) {
+                index = (l + h) >> 1;
+                uint256 o = index * size;
+                uint256 t = uint160(bytes20(SSTORE2.read(pointer, o, o + size)));
+
+                if (t == needle) {
+                    found = true;
+                    return (found, index);
+                } else if (t < needle) {
+                    l = index + 1;
+                } else {
+                    h = index;
+                }
+            }
+
+            // `index` will be zero in the case of an empty array,
+            // or when the value is less than the smallest value in the array.
+            return (false, index);
+        }
+    }
+
+    function _setLeadingByte(bytes32 data, uint8 b)
+        internal
+        virtual
+        pure
+        returns (bytes32 result)
+    {
+        /// @solidity memory-safe-assembly
+        assembly {
+            result := or(and(data, shr(8, not(0))), shl(248, b))
+        }
+    }
+
+    /// -----------------------------------------------------------------------
     /// ERC-721 Helpers
     /// -----------------------------------------------------------------------
 
-    function _mint(address to, uint256 id) internal virtual;
+    function _mint(address to, uint256 id) internal virtual {
+        if (to == address(0)) revert INVALID_RECIPIENT();
 
-    function _burn(uint256 id) internal virtual;
+        if (_safeOwnerOf(id) != address(0)) revert ALREADY_MINTED();
+
+        // Counter overflow is incredibly unrealistic.
+        unchecked {
+            _accountData[to]++;
+        }
+
+        _tokenData[id] = bytes32(bytes20(to));
+
+        emit Transfer(address(0), to, id);
+    }
+
+    function _burn(uint256 id) internal virtual {
+        address owner = _safeOwnerOf(id);
+
+        if (owner == address(0)) revert NOT_MINTED();
+
+        // Ownership check above ensures no underflow.
+        unchecked {
+            _accountData[owner]--;
+        }
+
+        delete _tokenData[id];
+
+        delete getApproved[id];
+
+        emit Transfer(owner, address(0), id);
+    }
 
     function _safeMint(address to, uint256 id) internal virtual {
         _mint(to, id);
